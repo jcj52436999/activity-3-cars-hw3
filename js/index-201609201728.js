b4c19947fdbde3b756bcac6efe7e083bfbd1115e
Joe Jackson <jcjackson@joe-jackson.net>
2016-09-20 22:43:09 -0700

    changed to secure sites for dependancies

--- js/index-201609201728.js
+++ js/index-201609201728.js
@@ -9,51 +9,36 @@ var canvasWidth = view.size.width;
 /*------------------------------------------*/
 
 //define makeCar function:
-var makeCar = function ( x, y, colorBod, styleBod, scaleBodX, flipBodY, signChoicesArray, clickChoicesArray ) {
+var makeCar = function ( x, y, colorBod, styleBod, scaleBodX, flipBodY ) {
     // colorBod = 'red';
     // sedan, putfslb
-    var layerArray = [];
+
 
     var carBody = new Path.Rectangle({
         point: [0, 20],
         size: [100, 25],
         fillColor: colorBod
     });
-    layerArray.push(carBody);
 
     var carTop = new Path.Rectangle({
         point: [15, 0],
         size: [50, 25],
         fillColor: colorBod
     });
-    layerArray.push(carTop);
 
     var wheel1 = new Path.Circle({
         center: new Point(75, 45),
         radius: 12,
         fillColor: '#444'
     });
-    layerArray.push(wheel1);
 
     var wheel2 = new Path.Circle({
         center: new Point(24, 45),
         radius: 12,
         fillColor: '#444'
     });
-    layerArray.push(wheel2);
 
-    if(signChoicesArray[0] != 'noSign'){
-    var sign = new PointText(new Point(50, 20));
-    // alert("signChoicesArray[0] = " + signChoicesArray[0])
-          sign.justification = signChoicesArray[1];
-          sign.fillColor = signChoicesArray[2];
-          sign.content = signChoicesArray[0];
-          layerArray.push(sign);
-    }
 
-// alert("layerArray - ", layerArray);
-// console.log("layerArray - ", layerArray);
-/*
 function onMouseDown(event) {
 	console.log('You pressed the mouse!');
 }
@@ -65,24 +50,15 @@ function onMouseDrag(event) {
 function onMouseUp(event) {
 	console.log('You released the mouse!');
 }
-*/
+
     // Group all of the shapes into a single layer
     // (similar to how Photoshop and Illustrator work):
     var autoAssy = new Layer({   // autoAssy was layer
-        // children: [carTop, carBody, wheel1, wheel2, sign],   // layerArray
-        children: layerArray ,
+        children: [carTop, carBody, wheel1, wheel2],
         position: [ x, y ]  // ,
         // scale[scaleBod]
         // this.scale(scaleBod);
     });
-
-    if(clickChoicesArray[0] != 'noClick'){
-      autoAssy.onClick = function (event) {
-        console.log("redirecting via mouse");
-        alert("redirecting...");
-        window.location = "https://www.jcjacksonconsulting.com";
-    };
-    }
 
     // this.scale(scaleBod);
     autoAssy.scale(scaleBodX, flipBodY);
@@ -118,17 +94,11 @@ var moveCar = function (car, speed) {
 };
 
 //call makeCar function:
-var car1 = makeCar( 0, 100, 'red', 'sedan', -.5, .5, ['noSign'], ['noClick']);
-var car2 = makeCar( 0, 200, 'blue', 'sedan', -.75, .75, ['noSign'], ['noClick']);
-var car3 = makeCar( 0, 300, 'green', 'sedan', 1.2, 1.2, ['noSign'], ['noClick']);
-var car4 = makeCar( 0, 400, 'orange', 'sedan', 2, -2, ['Driver \nin Training','center','black'], ['yesClick']);
-
-var put1 = makeCar(0, 450, 'yellow', 'putfslb', 2, 2, ['noSign'], ['noClick']);
-        /*
-        put1.sign.justification = 'center';
-        put1.sign.fillColor = 'white';
-        put1.sign.content = 'Driver \nin Training';
-        */
+var car1 = makeCar( 0, 100, 'red', 'sedan', -.5, .5);
+var car2 = makeCar( 0, 200, 'blue', 'sedan', -.75, .75);
+var car3 = makeCar( 0, 300, 'green', 'sedan', 1.2, 1.2);
+var car4 = makeCar( 0, 400, 'orange', 'sedan', 2, -2);
+var put1 = makeCar(0, 450, 'yellow', 'putfslb', 2, 2);
 
 // work but not needed at moment
 // car1.scale(.5);
